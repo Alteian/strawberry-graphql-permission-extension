22ade762df3ff11a274744f957a3554ca93b90a1
Alteian <Alteian@proton.me>
2023-07-16 03:51:09 +0200

    restrict imports to predefined classes/methods

--- strawberry_graphql_permission_extension/extensions/__init__.py
+++ strawberry_graphql_permission_extension/extensions/__init__.py
@@ -1,3 +1,5 @@
 from .base_extension import BasePermissionExtension
 from .has_permission_to_create import HasPermissionToCreate
 from .has_permission_to_interact import HasPermissionToInteract
+
+__all__ = ["BasePermissionExtension", "HasPermissionToCreate", "HasPermissionToInteract"]
--- strawberry_graphql_permission_extension/utils/__init__.py
+++ strawberry_graphql_permission_extension/utils/__init__.py
@@ -5,3 +5,12 @@ from .check_obj_permissions import PermissionCaseSwitcher
 from .has_permission_to_create_object import has_permission_to_create_object
 from .roles import Role
 from .enums import BaseOwnerFieldEnum
+
+
+__all__ = [
+    "get_global_id_objects_from_kwargs", 
+    "PermissionCaseSwitcher",
+    "has_permission_to_create_object",
+    "Role",
+    "BaseOwnerFieldEnum"
+    ]
